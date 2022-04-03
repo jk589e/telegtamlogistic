a8ffda30dd0ad318306e1917801342dc655c5037
jk58e9 <jk58e91@gmail.com>
2022-04-03 19:42:55 +0300

    add api files v 1.0

--- process_messages.py
+++ process_messages.py
@@ -1,10 +1,5 @@
-import requests as rq
 import warnings
-import json
-from pandas.io.json import json_normalize
 import pandas as pd
-import datetime as dt
-from  sqlalchemy import create_engine
 import time
 
 #import from local files
@@ -17,53 +12,6 @@ desired_width = 1000
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.max_rows', 5000)
-
-#database creds
-db_login = "globalvisor"
-db_password="Sinhrofazatron1"
-db_host = "rc1b-21imrctq167tcobb.mdb.yandexcloud.net"
-db_port = "6432"
-db_name = "telegramlogistics"
-
-
-#telegram bot config
-token = '5285834737:AAEQ5sIkdX2bdcAd6HQSjN1xzemp5UyMxgA'
-chat_id = '526697170'
-baseUrl = 'https://api.telegram.org/bot' + token + '/'
-headers = {'content-type': 'application/json'}
-def joinCol(A,a,B,b):
-    try:
-        A[a]=B[b]
-    except: pass
-
-
-sql =''' SELECT 
-	a.update_id, 
-	a.message_id, 
-	a.message_from_id, 
-	a.message_chat_id, 
-	a.inlinequery_id, 
-	a.callbackquery_id, 
-	a.username, 
-	a.first_name, 
-	a.last_name, 
-	a.message_text, 
-	a.message_date, 
-	a.latitude, 
-	a.longitude, 
-	a.processed,
-	b.status,
-	b.role_id,
-	b.company_id,
-	b.is_active,
-	b.registration_code
-FROM public.log_chat a left join public.users b on a.message_from_id = b.user_id 
-where a.processed is null
-order by update_id desc
-'''
-db_string = "postgresql://" + db_login + ":" + db_password + "@" + db_host + ":" + db_port + "/" + db_name
-
-engine = create_engine(db_string)
 
 
 while 1<2:
